frederickding/xenserver-kickstart

commit e8d713e2e2dcc9eebc06009054141bd0d4f4ecca

Enable SELinux and use plaintext root password
- Added note about using SHA512crypt

Signed-off-by: Frederick Ding <frederick@frederickding.com>

f20/f20-server.ks
@@ -23,12 +23,13 @@ firewall --disabled
 timezone --utc Etc/UTC
 
 # Authentication
-# use a SHA512crypted password; default here is 'Asdfqwerty'
-rootpw --iscrypted $6$9dC4m770Q1o$FCOvPxuqc1B22HM21M5WuUfhkiQntzMuAV7MY0qfVcvhwNQ2L86PcnDWfjDd12IFxWtRiTuvO/niB0Q3Xpf2I.
+rootpw Asdfqwerty
+# if you want to preset the root password in a public kickstart file, use SHA512crypt e.g.
+# rootpw --iscrypted $6$9dC4m770Q1o$FCOvPxuqc1B22HM21M5WuUfhkiQntzMuAV7MY0qfVcvhwNQ2L86PcnDWfjDd12IFxWtRiTuvO/niB0Q3Xpf2I.
 authconfig --enableshadow --passalgo=sha512
 
-# SELinux
-selinux --disabled
+# SELinux enabled
+selinux --enabled
 
 # Disable anything graphical
 skipx

commit e648d8f1a88750aec2378bdff410c6e3e6306ce8

Fix SELinux 'enforcing' line
- Bug introduced in e8d713e2e2dcc9eebc06009054141bd0d4f4ecca

f20/f20-server.ks
@@ -33,7 +33,7 @@ rootpw Asdfqwerty
 authconfig --enableshadow --passalgo=sha512
 
 # SELinux enabled
-selinux --enabled
+selinux --enforcing
 
 # Disable anything graphical
 skipx